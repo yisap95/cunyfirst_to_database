65420ce164b46578fa989c8ef68fa9145f3ee2da
Yigal Saperstein <yigal.saperstein@macaulay.cuny.edu>
2016-08-14 16:19:31 -0400

    what does heroku see

--- worker.js
+++ worker.js
@@ -280,7 +280,7 @@ getSession = function (inst, callback){
 
 getInst = function(callback){
     request.post(options, function(err, res, body) {
-        console.log('yo')
+        logger.log('yom')
         try{
             if(body.length == 0) return;
         } catch(err){
