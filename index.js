78a3b6bc012d92d45118ca809fdd267872793b36
Yigal Saperstein <yigal.saperstein@macaulay.cuny.edu>
2017-04-06 16:20:38 -0400

    tried changing

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ var pg = require('pg'); // for postgres access
 
 //runnning serrverrrr
 var http = require("http");
-var express = require('express'); // for creating the application, classic lib for node
-var session = require('express-session');
-var app = express()
+//var express = require('express'); // for creating the application, classic lib for node
+//var session = require('express-session');
+//var app = express()
 var port = process.env.PORT || 5000;
 var server = http.createServer(app)
-server.listen(port)
+//server.listen(port)
 
 
 // TODO: load into database here
